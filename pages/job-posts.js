04abb2e11ae8a9ba8d4644df4efe2b7acae2b02e
Josh Stroud <josh@agilend.net>
2023-04-16 10:52:34 -0700

    Add notes for the advertising manager role

--- pages/job-posts.js
+++ pages/job-posts.js
@@ -8,6 +8,21 @@ export default function JobPosts() {
         <p>Some notes:</p>
         <p>Probably going to be someone internal to the house, but does not live there. Someone who is willing to take on a function, and step up in to a leadership role.</p>
         <p>We are positioning the house as something provactive, subtle, and interactive. We take cues from movements like Consciousness Hacking (now defunct), and other experiential movements in the broader technology community. We aim to create change through direct experience, through media, and through technology. We believe in demos and press to bring it all together.</p>
+        
+        <h2>Some various notes in no particular order</h2>
+        <h3>Advertising Manager Notes:</h3>
+        <ul>
+            <li>* Referrals suck. They take away control from the ownership team.</li>
+            <li>* Social media sucks. No reason here yet.</li>
+            <li>* PR sucks. We will never earn it, either micro or macro.</li>
+            <li>* Events suck. Too much time and they never lead to the right people, post Pandemic.</li>
+        </ul>
+        <p>What's the answer? The answer is <strong>lead gen through content marketing</strong> aka SEO.</p>
+        <p>Ah, the dreaded SEO... the thing that strikes fear into the heart of marketers across the tech world. How do you rank on Google? Do you use evil voodoo tricks and black magic?</p>
+        <p>Alas, it's not an evil formula... it's just effort, and doing the right moves over a long period of time. There <em>is</em> a shorter way. Here it is:</p>
+        <p>Guest posts... all we need is guest posts for marketing. Here's the trick: </p>
+        <p>We only need one guest post, that ranks well, for one keyword. If we get that, we get a steady stream of leads, and a steady stream of new housing customers.</p>
+        <p>Signing off... April 16, 2023 10:51 Pacific</p>
         </main>
         </>
     )
