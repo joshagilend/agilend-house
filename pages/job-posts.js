e53764b3486a3b82cd721d6864ff0b6e63120503
Josh Stroud <josh@agilend.net>
2023-04-16 10:43:32 -0700

    Add draft job posts page, pretty bad but what we've got so far

---
+++ pages/job-posts.js
@@ -0,0 +1,14 @@
+export default function JobPosts() {
+    return (
+        <>
+        <main>
+        <h1>Job Postings (unpaid)</h1>
+        <h2>Advertising Manager for Dragon Toon</h2>
+        <p>This position is to advertise our housing opportunity on 3rd party websites like Zillow, Craigslist, and Facebook Marketplace and Groups. Also, Reddit, AI forums, and other social media. </p>
+        <p>Some notes:</p>
+        <p>Probably going to be someone internal to the house, but does not live there. Someone who is willing to take on a function, and step up in to a leadership role.</p>
+        <p>We are positioning the house as something provactive, subtle, and interactive. We take cues from movements like Consciousness Hacking (now defunct), and other experiential movements in the broader technology community. We aim to create change through direct experience, through media, and through technology. We believe in demos and press to bring it all together.</p>
+        </main>
+        </>
+    )
+}
